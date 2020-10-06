206f018acf0a5ea08f6a36e2e42c36b898c76602
tumichen <461049364@qq.com>
2020-10-06 22:42:53 +0800

    save code

---
+++ sp04-orderservice/src/main/java/cn/tedu/sp04/order/feignclient/UserFeignService.java
@@ -0,0 +1,18 @@
+package cn.tedu.sp04.order.feignclient;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import cn.tedu.sp01.pojo.User;
+import cn.tedu.web.util.JsonResult;
+
+@FeignClient(name="user-service", fallback = UserFeignServiceFB.class)
+public interface UserFeignService {
+	@GetMapping("/{userId}")
+	JsonResult<User> getUser(@PathVariable("userId") Integer userId);
+
+	@GetMapping("/{userId}/score") 
+	JsonResult addScore(@PathVariable("userId") Integer userId, @RequestParam("score") Integer score);
+}
